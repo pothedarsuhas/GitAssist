c85c6a929d12b12244d64f1b49d511ec3f0a0ba2
pothedarsuhas <pothedarsuhas@gmail.com>
2019-03-05 10:37:52 +0530

    Update dummy.py

--- dummy.py
+++ dummy.py
@@ -39,3 +39,50 @@ g = Github("pothedarsuhas","XXXXXXXXXXXXx")
 # base = repo.get_branch("master")
 # head = repo.get_branch(WORKING_BRANCH)
 # merge_to_master = repo.merge("master", head.commit.sha, "merge to master")
+
+
+from github import *
+from git import Repo
+import subprocess
+
+# First create a Github instance:
+
+# using username and password
+g = Github("pothedarsuhas","XXXXXXXXXXXXx")
+
+# or using an access token
+# g = Github("XXXXXXXXXXXXXXXXXXXXX")
+
+# Github Enterprise with custom hostname
+# g = Github(base_url="https://{hostname}/api/v3", login_or_token="access_token")
+
+# Then play with your Github objects:
+# for repo in g.get_user().get_repos():
+#     print(repo.name)
+
+# repo = g.get_repo('pothedarsuhas/Python')
+# contents = repo.get_contents("")
+# for content_file in contents:
+#     print(content_file)
+
+# repo = g.get_repo("pothedarsuhas/GitAssist")
+# a = str(list(repo.get_branches()))
+# print(a)
+
+
+# repoName = "Jenkins" #repo
+# source_branch = 'master'
+# target_branch = 'newbranch' #ref
+# repo = g.get_user().get_repo(repoName)
+# sb = repo.get_branch(source_branch)
+# repo.create_git_ref('refs/heads/' + target_branch, sb.commit.sha)
+# print(repo.get_git_ref('heads/master'))
+
+# owner = "pothedarsuhas"
+# repository = "Jenkins"
+# git_url = "https://github.com/"+owner+'/'+repository+'.git'   # https://github.com/pothedarsuhas/Jenkins.git
+# repo_dir = "/Users/suhaspothedar/Downloads/"+repository
+# branch = 'master'
+# Repo.clone_from(git_url, repo_dir,branch= branch)
+
+
